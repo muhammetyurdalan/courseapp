2082316200607bef83ca58d86958d6eb1b9678d0
muhammetyurdalan <muhammetyurdalan7@gmail.com>
2023-11-02 01:03:28 +0300

    migrate işlemi yapıldı db ye Course model eklendi

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+# Create your models here..
+class Course(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.TextField()
+    imageUrl=models.CharField(max_length=50)
+    date=models.DateField(datetime.now())
+    isActive=models.BooleanField()
+    
+    
+    def __str__(self):
+        return f"{self.title}"
