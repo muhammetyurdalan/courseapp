b54fb7beb02efbdb13c411fabed1f9585b981f6c
muhammetyurdalan <muhammetyurdalan7@gmail.com>
2023-11-06 10:51:32 +0300

    admin sayfası düzenlendi

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Course,Categories
 
 # Register your models here.
+
+
+#Admin panelini özelleştirme 1.yöntem
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    #ekranda gösterilecek kolonları seçiyoruz
+    list_display=("title","isActive","slug","isUptaded")
+    #tıklanınca edit sayfasına yönlendir
+    list_display_links=("title","slug")
+    #sadece okunabilir yapma
+    readonly_fields=("slug",)
+    #yan tarafta çıkacak filtreleme listesi
+    list_filter=("isActive","isUptaded")
+    #edit sayfasına gitmeden düzenleyebilmek için
+    list_editable=("isActive","isUptaded")
+    #arama textfielde içinde neleri arayabileceğimiz
+    search_fields=("title","description")
+
+class CateoriesAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Categories,CateoriesAdmin)
--- courses/models.py
+++ courses/models.py
@@ -9,7 +9,9 @@ class Course(models.Model):
     imageUrl=models.CharField(max_length=50)
     date=models.DateField(datetime.now())
     isActive=models.BooleanField()
-    slug=models.SlugField(default="",null=False,unique=True,db_index=True)
+    slug=models.SlugField(default="",blank=True,editable=False,null=False,unique=True,db_index=True)
+    #blank form esnasında boş geçilebilmesi için true
+    #editable=false ise hiç sorulmamasını sağlar
     isUptaded=models.BooleanField(default=True)
     
     
@@ -22,6 +24,15 @@ class Course(models.Model):
         return f"{self.title}"
     
 class Categories(models.Model):
-    id=models.IntegerField(primary_key=True)
     name=models.CharField(max_length=50)
-    slug=models.SlugField()
+    slug=models.SlugField(default="",blank=True,editable=False,null=False,unique=True,db_index=True)
+    
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.name)
+        super().save(args,kwargs)
+    
